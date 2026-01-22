c054e260be604098a21ff8c5bbb6984cecec83e4
arpit sharma <arpit02456@gmail.com>
2026-01-22 09:49:54 +0530

    Refactor project into frontend and backend folders

--- src/utils/jwt.ts
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const generateAccessToken = (id: number) =>
-  jwt.sign({ id }, process.env.ACCESS_SECRET!, { expiresIn: "15m" });
-
-export const generateRefreshToken = (id: number) =>
-  jwt.sign({ id }, process.env.REFRESH_SECRET!, { expiresIn: "7d" });
